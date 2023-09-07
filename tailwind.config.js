08c272e3e30da5c48aeb9623f90a128ac2baf95b
AbogeJr <junioraboge@gmail.com>
2023-09-08 00:17:09 +0300

    update: initialize repo && config

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        blue: "#3783B4",
+        green: "#1C9346",
+        orange: "#F7A01C",
+        red: "#DB2127",
+      },
+    },
+    plugins: [],
+  },
+};
